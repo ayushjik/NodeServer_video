80455d66f36349b1e8b99834b6dfbe3355a2fe91
ayushjik <112191793+ayushjik@users.noreply.github.com>
2022-12-23 18:09:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
 const app = require("express")();
-// const server = require("http").createServer(app);
-var httpServer = http.createServer(app);
-var privateKey  = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('server.crt', 'utf8');
-// var privateKey  = fs.readFileSync('../../../key.pem', 'utf8');
-// var certificate = fs.readFileSync('./server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
-var httpsServer = https.createServer(credentials, app);
-
+const server = require("http").createServer(app);
 const cors = require("cors");
 
-const io = require("socket.io")(httpsServer, {
+const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
@@ -22,7 +11,7 @@ const io = require("socket.io")(httpsServer, {
 
 app.use(cors());
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
@@ -30,24 +19,21 @@ app.get('/', (req, res) => {
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
+	console.log("socket.id:- "+socket.id)
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
 	});
 
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		console.log("answerCall:-"+userToCall+" signalData:- "+signalData+" from:- "+from+" name" );
+		console.log("callUser"+userToCall+" SignalData:-"+signalData+" from:-"+from+" name:-"+name)
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
 
 	socket.on("answerCall", (data) => {
-		console.log("answerCall:-"+data+" dataValue:-"+data.value+" dataObject:-"+ data.object);
+		console.log("data:-"+data +" data.signal:-"+data.signal)
 		io.to(data.to).emit("callAccepted", data.signal)
 	});
-	socket.on("message",(data2)=>{
-		socket.broadcast.emit('message',data2)
-		console.log("its Work for sending",data2)
-	})
 });
-httpServer.listen(5001, () => console.log(`Server is running on port 5001`));
-httpsServer.listen(5000, () => console.log(`Server is running on port 5000`));
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
