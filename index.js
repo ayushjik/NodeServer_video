c05d2b564161a55c25155fd627a0c0ce209a0497
ayushjik <112191793+ayushjik@users.noreply.github.com>
2022-12-23 13:22:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 const app = require("express")();
-const server = require("http").createServer(app);
+// const server = require("http").createServer(app);
+var httpServer = http.createServer(app);
+var privateKey  = fs.readFileSync('../../../key.pem', 'utf8');
+var certificate = fs.readFileSync('./server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+
 const cors = require("cors");
 
-const io = require("socket.io")(server, {
+const io = require("socket.io")(httpsServer, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
@@ -11,7 +20,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
@@ -25,12 +34,18 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		console.log("answerCall:-"+userToCall+" signalData:- "+signalData+" from:- "+from+" name" );
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
 
 	socket.on("answerCall", (data) => {
+		console.log("answerCall:-"+data+" dataValue:-"+data.value+" dataObject:-"+ data.object);
 		io.to(data.to).emit("callAccepted", data.signal)
 	});
+	socket.on("message",(data2)=>{
+		socket.broadcast.emit('message',data2)
+		console.log("its Work for sending",data2)
+	})
 });
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+httpServer.listen(5001, () => console.log(`Server is running on port 5001`));
+httpsServer.listen(5000, () => console.log(`Server is running on port 5000`));
